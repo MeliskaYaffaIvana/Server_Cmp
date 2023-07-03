3208d6e3cd4cd2b17f04677cd92371dc8ef7870d
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-03 14:56:20 +0700

    coba

--- cmp/views.py
+++ cmp/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.db import connection
 import subprocess
 
 @csrf_exempt
@@ -22,11 +21,6 @@ def create_template(request):
             'message': 'Template created successfully.'
         }
 
-        # # Mengubah status_job menjadi 0 di database
-        # with connection.cursor() as cursor:
-        #     update_query = "UPDATE template SET status_job = 0 WHERE  nama_template = %s"
-        #     cursor.execute(update_query, [nama_template])
-
         return JsonResponse(response)
 
     else:
@@ -35,3 +29,42 @@ def create_template(request):
             'message': 'Invalid request method.'
         }
         return JsonResponse(response)
+
+
+# from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from django.db import connection
+# import subprocess
+
+# @csrf_exempt
+# def create_template(request):
+#     if request.method == 'POST':
+#         # Mengambil inputan dari permintaan klien
+#         nama_template = request.POST.get('nama_template')
+#         link_template = request.POST.get('link_template')
+
+#         # Perintah untuk membuat images Docker dari link Docker Hub
+#         docker_cmd = f"docker pull {link_template} && docker tag {link_template} {nama_template}"
+
+#         # Menjalankan perintah menggunakan subprocess
+#         subprocess.run(docker_cmd, shell=True)
+
+#         # Mengirimkan respon ke klien
+#         response = {
+#             'status': 'success',
+#             'message': 'Template created successfully.'
+#         }
+
+#         # # Mengubah status_job menjadi 0 di database
+#         # with connection.cursor() as cursor:
+#         #     update_query = "UPDATE template SET status_job = 0 WHERE  nama_template = %s"
+#         #     cursor.execute(update_query, [nama_template])
+
+#         return JsonResponse(response)
+
+#     else:
+#         response = {
+#             'status': 'error',
+#             'message': 'Invalid request method.'
+#         }
+#         return JsonResponse(response)
