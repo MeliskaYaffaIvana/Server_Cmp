b9997d4c9699a4edb4b13de466fce63b8046228f
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-03 16:37:37 +0700

    coba

--- cmp/views.py
+++ cmp/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import subprocess
+import subprocess
 
 @csrf_exempt
 def create_template(request):
@@ -8,13 +9,18 @@ def create_template(request):
         # Mengambil inputan dari permintaan klien
         nama_template = request.POST.get('nama_template')
         link_template = request.POST.get('link_template')
-        print (nama_template)
-        print (link_template)
-        # # Mengubah nama_template menjadi lowercase
-        # nama_template = nama_template.lower()
-        
-        # Perintah untuk membuat images Docker dari link Docker Hub
-        docker_cmd = f"docker pull {link_template}"
+        print(nama_template)
+        print(link_template)
+
+        # Parsing the repository and tag from the link
+        repository, tag = link_template.split(':')
+        repository = repository.lower()  # Converting repository name to lowercase
+
+        # Creating the new image reference with lowercase repository name
+        new_image_ref = f"{repository}:{tag}"
+
+        # Perintah untuk melakukan docker pull dengan image reference yang sudah dimodifikasi
+        docker_cmd = f"docker pull {new_image_ref} && docker tag {new_image_ref} {nama_template}"
 
         # Menjalankan perintah menggunakan subprocess
         subprocess.run(docker_cmd, shell=True)
@@ -33,7 +39,6 @@ def create_template(request):
             'message': 'Invalid request method.'
         }
         return JsonResponse(response)
-
 
 
 # from django.http import JsonResponse
