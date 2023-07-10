aa8bbab75e0c5daec36baf1fe1a53b45077b5729
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-10 20:09:17 +0700

    .

--- cmp/views.py
+++ cmp/views.py
@@ -29,6 +29,7 @@ def update_bolehkan(request):
         status = result.stdout.strip()
         print(status)
         cmd_stop = None
+        cmd_start = None
         # Menentukan status kontainer berdasarkan nilai bolehkan
         if bolehkan == '0' and status == 'running':
             # Jika bolehkan 0 dan status running, menjalankan perintah Docker stop
